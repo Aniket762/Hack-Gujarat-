91e12285bc1da4960fc6e572311170b26596157c
Aniket <aniketindian8@gmail.com>
2020-09-05 15:14:33 +0530

    c# files

---
+++ Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemController : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Animator anim;
+
+	public AudioSource rageaudio;
+	public AudioSource jumpaudio;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		anim = GetComponent<Animator>();
+		rageaudio= GameObject.Find("RageSound").GetComponent<AudioSource>();
+		jumpaudio= GameObject.Find("JumpSound").GetComponent<AudioSource>();
+	}
+
+	public void GolemActions(string ActionCommands)
+	{
+		ActionCommands = ActionCommands.Trim();
+        switch (ActionCommands)
+		{
+          case "jump":
+		   anim.Play("jump",-1,0f);
+		   jumpaudio.Play(11000);
+		   break;
+		  case "anger":
+		   anim.Play("rage",-1,0f);
+		   rageaudio.Play();
+		   break;
+
+		  default:
+		   anim.Play("idle",-1,0f);
+		   break; 
+
+		}
+        
+
+	}
+}
